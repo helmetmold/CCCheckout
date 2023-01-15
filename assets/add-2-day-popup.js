cdc60d7d4f58122c30c811c63ecbeadcc015d357
Kai Forman <62826558+helmetmold@users.noreply.github.com>
2023-01-15 15:57:02 +1000

    Update add-2-day-popup.js

--- assets/add-2-day-popup.js
+++ assets/add-2-day-popup.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 2a437a232f17e786e6cf1f413aa5693f4d116559
->>>>>>> 08e066ba998841a11e197df47bd442bb4ec8763f
 var SubmitCartButton = document.getElementsByClassName('product-form__submit');
 
 for (let index = 0; index < SubmitCartButton.length; index++) 
@@ -21,29 +14,3 @@ function OpenUpsellPopUp()
 
 
 //get new cart button when made
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
-var SubmitCartButton = document.getElementsByClassName('product-form__submit');
-
-for (let index = 0; index < SubmitCartButton.length; index++) 
-  {
-    SubmitCartButton[index].addEventListener("click", OpenUpsellPopUp);
-  }
-
-function OpenUpsellPopUp() 
-{
-  console.log("open upsell");
-  var ProductQuickAddButton = document.getElementsByClassName('quick-add__submit');
-  ProductQuickAddButton[0].click();
-}
-
-
-//get new cart button when made
-
->>>>>>> a83d7143c91e85b03e0aec55ad48641a3d6dfbd3
->>>>>>> 2a437a232f17e786e6cf1f413aa5693f4d116559
->>>>>>> 08e066ba998841a11e197df47bd442bb4ec8763f
