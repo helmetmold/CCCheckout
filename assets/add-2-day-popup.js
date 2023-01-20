335d52636b7f3f9d7411c6b69519c5299b5dd84e
Kai Forman <62826558+helmetmold@users.noreply.github.com>
2023-01-20 16:49:49 +1000

    product changes

--- assets/add-2-day-popup.js
+++ assets/add-2-day-popup.js
@@ -7,10 +7,48 @@ for (let index = 0; index < SubmitCartButton.length; index++)
 
 function OpenUpsellPopUp() 
 {
-  console.log("open upsell");
   var ProductQuickAddButton = document.getElementsByClassName('quick-add__submit');
+  AddExtras();
   ProductQuickAddButton[0].click();
+  ProductQuickAddButton[1].click();
 }
 
-
+function AddExtras() 
+{
+  var extras = document.getElementsByClassName('Extras');
+  for (let index = 0; index < extras.length; index++) 
+  {
+    if (extras[index].value == "Regular") 
+    {
+      var ProductQuickAddButton = document.getElementsByClassName('quick-add__submit');
+      
+    } 
+    else if (extras[index].value == "Vegetarian") 
+    {
+      var product = document.getElementById("Lunch (3-day)");
+      product.click();
+    }
+    else if (extras[index].value == "Standard iPad rental") 
+    {
+      console.log("Standard iPad rental");
+    }
+    else if (extras[index].value == "iPad rental with insurance") 
+    {
+      console.log("iPad rental with insurance");
+    }
+    else if (extras[index].value == "Early Drop-Off") 
+    {
+      console.log("Early Drop-Off");
+    }
+    else if (extras[index].value == "Late Pick-up") 
+    {
+      console.log("Late Pick-up");
+    }
+    else if (extras[index].value == "Early Drop-Off and Late Pick-up") 
+    {
+      console.log("Early Drop-Off and Late Pick-up");
+    }
+  }
+  
+}
 //get new cart button when made
