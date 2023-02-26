465ff9c170e23ff7a6cf790891ca15858e613e5e
Kai Forman <62826558+helmetmold@users.noreply.github.com>
2023-02-26 20:13:06 +1000

    changes made

--- assets/global.js
+++ assets/global.js
@@ -543,12 +543,15 @@ class ModalDialog extends HTMLElement {
         if (
           event.pointerType === "mouse" &&
           !event.target.closest("deferred-media, product-model")
-        )
-          this.hide();
+        ) {
+        }
+        //this.hide();
       });
     } else {
       this.addEventListener("click", (event) => {
-        if (event.target === this) this.hide();
+        if (event.target === this) {
+          //this.hide();
+        }
       });
     }
   }
